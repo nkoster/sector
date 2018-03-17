152981d02278f4e9e4358a1d47083c314e43ae1b
Niels Koster <n.koster@portavita.eu>
2018-03-17 04:23:57 +0100

    "drive" instead of "diskName".

--- sector.cpp
+++ sector.cpp
@@ -8,13 +8,13 @@ int main(int argc, char **argv) {
     std::cout << "\nUsage: sudo " << argv[0] << " <disk> [sector] (default is 0)\n\n";
     return 1;
   }
-  std::string diskName = std::string() + argv[1];
+  std::string drive = std::string() + argv[1];
   std::string sector = std::string() + "0";
   if (argc > 2) sector += std::string() + argv[2];
   std::string::size_type sz;
   int s = std::stoi (sector, &sz);
-  std::string diskError = std::string() + diskName + ": ";
-  std::ifstream disk(diskName, std::ios_base::binary);
+  std::string diskError = std::string() + drive + ": ";
+  std::ifstream disk(drive, std::ios_base::binary);
   if(!disk) return 2;
   disk.seekg(512 * s);
   if(!disk) return 3;
