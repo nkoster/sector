fed13fde4492d48d2b47b6a236977992fce92741
Niels Koster <n.koster@portavita.eu>
2018-03-17 04:00:52 +0100

    Less lines.

--- sector.cpp
+++ sector.cpp
@@ -10,25 +10,17 @@ int main(int argc, char **argv) {
   }
   std::string diskName = std::string() + argv[1];
   std::string sector = std::string() + "0";
-  if (argc > 2) {
-    sector += std::string() + argv[2];
-  }
+  if (argc > 2) sector += std::string() + argv[2];
   std::string::size_type sz;
   int s = std::stoi (sector, &sz);
   std::string diskError = std::string() + diskName + ": ";
   std::ifstream disk(diskName, std::ios_base::binary);
-  if(!disk) {
-      return 2;
-  }
+  if(!disk) return 2;
   disk.seekg(512 * s);
-  if(!disk) {
-      return 3;
-  }
+  if(!disk) return 3;
   std::vector<char> buffer(512);
   disk.read(&buffer[0], 512);
-  if(!disk) {
-      return 4;
-  }
+  if(!disk) return 4;
   for (auto i = buffer.begin(); i != buffer.end(); ++i)
     std::cout << *i;
 }
